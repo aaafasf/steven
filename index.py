d95a8072401d8d9c22d4c7f29717964045dac4ae
aaafasf <csc.calle@yavirac.edu.ec>
2025-11-07 15:17:47 -0500

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Servidor Flask funcionando correctamente en Render 😎"
+    return "El "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
