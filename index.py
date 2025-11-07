9ee4a766b9b57a570eeb4197ac76e8463b9645d7
aaafasf <csc.calle@yavirac.edu.ec>
2025-11-07 14:57:35 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
+import time
+
 def sumar(a, b):
     return a + b
 
 if __name__ == "__main__":
     print("Resultado:", sumar(2, 3))
+    print("Servidor corriendo en Render...")
+    while True:
+        time.sleep(60)
